a3d85acb9580bb902033e553de37e1bd95b497cc
amgs <user@example.com>
2024-10-07 15:08:04 +0100

    Criação de ficheiro com resolução de problemas.

---
+++ AulaCiclos/Problems.cs
@@ -0,0 +1,31 @@
+class Problems { // Não sabemos para que serve e/ou como funciona (ainda)
+    public static void Problem1() { // idem
+        bool exit = false;
+        while (!exit) {
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1. Quadrado");
+            Console.WriteLine("2. Triângulo");
+            Console.WriteLine("3. Círculo");
+            Console.WriteLine("4. Sair");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option) {
+                case 1:
+                    Console.WriteLine("Área = lado x lado");
+                    break;
+                case 2:
+                    Console.WriteLine("Área = base x altura / 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Área = 2 x pi x raio");
+                    break;
+                case 4:
+                    Console.WriteLine("Até breve!");
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+    }
+}
