f6f3c9a4698800d41144318ba63a35dd4762ec40
Tiago Lança <tiagofilipe.lanca@gmail.com>
2024-10-16 17:09:22 +0100

    Exercicios1-7

--- AulaCiclos/Program.cs
+++ AulaCiclos/Program.cs
@@ -1,6 +1,9 @@
-﻿// Problems.Problem1();
-// Problems.Problem2();
-// Problems.Problem3();
-// Problems.Problem4();
-// Problems.Problem5();
-Problems.Problem6a();
+﻿//Problems.Problem2();
+//Problems.Problem3();
+//Problems.Problem4();
+//Problems.Problem5();
+//Problems.Problem6a();
+//Problems.Problem6b();
+//Problems.Problem6c();
+//Problems.Problem6d();
+//Problems.Problem7();
